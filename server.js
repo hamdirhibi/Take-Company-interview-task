404ab0037190f5a695e94243cecf80f56177c77d
geekrhibi <geekrhibi@gmail.com>
2020-11-01 21:57:06 +0100

    deployment configuration

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/smart-contact'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: './dist/smart-contact'});
+  });
+  
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
